fd3e9da31d9f67be21fc36f209e4d42412567ef5
chandan <chandan@chandanws>
2017-04-15 20:56:56 -0400

    new VO

---
+++ PDM/src/main/java/com/sample/PDMFormData.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+import java.math.BigInteger;
+
+public class PDMFormData {
+	private BigInteger id;
+	private String usageNotes;
+	public BigInteger getId() {
+		return id;
+	}
+	public void setId(BigInteger id) {
+		this.id = id;
+	}
+	public String getUsageNotes() {
+		return usageNotes;
+	}
+	public void setUsageNotes(String usageNotes) {
+		this.usageNotes = usageNotes;
+	}
+	
+	
+}
